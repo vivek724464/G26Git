77e138f628f433f8b627851d9ef44ccb79b46c8f
Vivek Kumar <vivekbxr30@gmail.com>
2025-11-24 15:01:04 +0530

    model created

---
+++ model/userSchema.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name:{type:String, require:true},
+    email:{type:String, require:true},
+    password:{type:String, require:true},
+    contact:{type:Number}
+})
+    
+module.exports = mongoose.model("User", userSchema);
