d602008678a5412afa924bcd01e9650c958257e5
Vivek Kumar <vivekbxr30@gmail.com>
2025-11-25 14:35:08 +0530

    user routes created

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express=require("express");
-const mongoose=require("mongoose")
+const mongoose=require("mongoose");
+const userRouter=require("./routes/userRoutes");
 const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use("api/user", userRouter);
 app.get("/", (req, res)=>{
     res.json("ok")
 })
